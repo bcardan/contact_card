06e06998ff377f36692b50b1adc68da2ac5b46d2
Bent Cardan <bcardan@gmail.com>
2011-09-26 23:31:49 -0400

    initial commit.

---
+++ index.php
@@ -0,0 +1,327 @@
+<!Doctype html>
+
+<!--[if lt IE 7]><html class="no-js ie6 oldie" lang=en><![endif]-->
+<!--[if IE 7]><html class="no-js ie7 oldie" lang=en><![endif]-->
+<!--[if IE 8]><html class="no-js ie8 oldie" lang=en><![endif]-->
+
+<!--[if gt IE 8]><!--> <html class=no-js lang=en> <!--<![endif]-->
+<head>
+  
+  
+
+  
+  
+  <link href='http://fonts.googleapis.com/css?family=Amaranth:400italic,700' rel=stylesheet type='text/css'>
+  
+  <meta http-equiv=X-UA-Compatible content="IE=edge,chrome=1">
+
+  <title>Bent Cardan - Font End Developer</title>
+  <meta name=description content="The personal site of Bent Cardan">
+  <meta name=author content="Bent Cardan">
+
+  
+  <meta name=viewport content="width=device-width,initial-scale=1">
+
+  
+
+  
+  <link rel=stylesheet href='./css/371577ff9e030278b7a459a98069eaa0a6e81595.css'>
+
+  
+
+  <script src="./js/libs/modernizr-2.0.6.min.js"></script>
+<meta charset="UTF-8"></head>
+
+<body>
+
+	<div id=settings>
+		<div class=button>
+			<img src="./img/icons/prefs.png" alt=Preferences />
+		</div>
+		<h1>Colors</h1>
+		<ul id=colors>
+			<li><div class=gray title=gray></div></li>
+			<li><div class=blue title=blue></div></li>
+			<li><div class=green title=green></div></li>
+			<li><div class=red title=red></div></li>
+			<li><div class=orange title=orange></div></li>
+			<li><div class=purple title=purple></div></li>
+		</ul>
+		<h1>Patterns</h1>
+		<ul id=patterns>
+			<li><div class=carbon title=carbon></div></li>
+			<li><div class=crosses title=crosses></div></li>
+			<li><div class=diagonal title=diagonal></div></li>
+			<li><div class=lines title=lines></div></li>
+			<li><div class=squares title=squares></div></li>
+			<li><div class=texture title=texture></div></li>
+		</ul>
+	</div>
+
+	
+	
+  <div id=container>
+  	
+  	
+    <hgroup id=name-header>
+		<h1><a href="">Bent <span class=surname-color>Cardan</span></a></h1>
+		<h2 class=mid-color-text><em>Front End Developer</em></h2>
+    </hgroup>
+    <div id=main class=light  role=main>
+    	<div class=outer-border></div>
+    	
+		<nav id=main-navigation class="dark semi-light-text">
+			<ul>
+				<li title=Home><a href="#home"><img src="./img/icons/UI/user.png" alt=Home /><img class=active-image src="./img/icons/UI/user_dark.png" alt=Home /></a></li>
+				<li title=Resume><a href="#resume"><img src="./img/icons/UI/document_fill.png" alt=Resume /><img class=active-image src="./img/icons/UI/document_fill_dark.png" alt=Resume /></a></li>
+				<li title=Portfolio><a href="#portfolio"><img src="./img/icons/UI/folder_fill.png" alt=Portfolio /><img class=active-image src="./img/icons/UI/folder_fill_dark.png" alt=Portfolio /></a></li>
+				<li title=Contact><a href="#contact"><img src="./img/icons/UI/mail_alt.png" alt=Contact /><img class=active-image src="./img/icons/UI/mail_alt_dark.png" alt=Mail /></a></li>
+			</ul>
+		</nav>
+		<div class=scroll-wrapper>
+			
+			
+	    	<section id=home class=semi-dark-text>
+	    		
+	    		<article id=about>
+	    			<div class=inner-border></div>
+	    			<h1 class=dark-text>About Me</h1>
+	    			
+	    			<aside><img src="./img/profile.jpg" alt=Profile /></aside>
+	    			<p>
+	    				Every art and every inquiry, and similarly every action and pursuit, is thought to aim at some good; and for this reason the good has rightly been declared to be that at which all things aim. But a certain difference is found among ends; 
+					</p>
+					<p class=secondary>
+						Where there are ends apart from the actions, it is the nature of the products to be better than the activities. Now, as there are <a class=text-link href="#">many actions</a>, arts, and sciences, their ends also are many; Now, as there are <a class=text-link href="#">many actions</a>, arts, and sciences, their ends also are many; Now, as there are <a class=text-link href="#">many actions</a>, arts, and sciences, their ends also are many;
+					</p>
+	    		</article>
+	    		<div id=social-links>
+		    		<div class=inner-border></div>
+		    		
+		    		<ul>
+		    			<li><a href="//facebook.com/bentcardan.com"><img src="./img/icons/social/icon_Facebook.png" alt=Facebook />Facebook</a></li>
+		    			<li><a href="#"><img src="./img/icons/social/icon_Twitter (alt).png" alt=Twitter />Twitter</a></li>
+		    			<li><a href="#"><img src="./img/icons/social/icon_LinkedIn (alt).png" alt=LinkedIn />Linked In</a></li>
+		    			<li><a href="#"><img src="./img/icons/social/icon_Flickr.png" alt=Flickr />Flickr</a></li>
+		    			<li><a href="#"><img src="./img/icons/social/icon_Vimeo.png" alt=Vimeo />Vimeo</a></li>
+		    			<li><a href="#"><img src="./img/icons/social/icon_Dribbble.png" alt=Dribbble />Dribbble</a></li>
+		    		</ul>
+		    	</div>
+	    	</section>
+	    	
+	    	
+	    	<section id=resume class=semi-dark-text>
+	    		
+	    		<div class=left-column>
+	    			<div class=inner-border></div>
+	    			<h1 class=dark-text>My Skillset</h1>
+	    			
+		    		<div id=skillset>
+		    			
+		    			<ul class=bar-graph>
+		    				<li data-graph-percent=100><span class=bar-title>HTML/CSS</span>
+								<aside>
+									<p>
+									But where such arts fall under a single capacity- as bridle-making and the other arts concerned with the equipment of horses fall under the art of riding,</p> 
+									<p class=secondary>
+									<a href="#">and this and</a> every military action under strategy, in the same way other arts fall under yet others- in all of these the ends of the master arts are to be preferred to all the subordinate ends;</p>
+
+								</aside>
+							</li>
+							<li data-graph-percent=70><span class=bar-title>PHP and MySQL</span>
+								<aside>
+									<p>
+									But where such arts fall under a single capacity- as bridle-making and the other arts concerned with the equipment of horses fall under the art of riding,</p> 
+									<p class=secondary>
+									<a href="#">and this and</a> every military action under strategy, in the same way other arts fall under yet others- in all of these the ends of the master arts are to be preferred to all the subordinate ends;</p>
+								</aside>
+							</li>
+		    				<li data-graph-percent=100><span class=bar-title>Javascript</span>
+								<aside>
+									<p>
+									But where such arts fall under a single capacity- as bridle-making and the other arts concerned with the equipment of horses fall under the art of riding,</p> 
+									<p class=secondary>
+									<a href="#">and this and</a> every military action under strategy, in the same way other arts fall under yet others- in all of these the ends of the master arts are to be preferred to all the subordinate ends;</p>								</aside>
+							</li>
+		    				<li data-graph-percent=75><span class=bar-title>Photoshop</span>
+								<aside>
+									<p>
+									But where such arts fall under a single capacity- as bridle-making and the other arts concerned with the equipment of horses fall under the art of riding,</p> 
+									<p class=secondary>
+									<a href="#">and this and</a> every military action under strategy, in the same way other arts fall under yet others- in all of these the ends of the master arts are to be preferred to all the subordinate ends;</p>								</aside>
+							</li>
+							<li data-graph-percent=40><span class=bar-title>Ruby on Rails</span>
+								<aside>
+									<p>
+									But where such arts fall under a single capacity- as bridle-making and the other arts concerned with the equipment of horses fall under the art of riding,</p> 
+									<p class=secondary>
+									<a href="#">and this and</a> every military action under strategy, in the same way other arts fall under yet others- in all of these the ends of the master arts are to be preferred to all the subordinate ends;</p>								</aside>
+							</li>
+		    			</ul>
+		    		</div>
+		    		<div id=work> 
+		    			<h1>What I Do</h1> 
+		    			<ul> 
+		    				<li>Web Front-End Development</li> 
+		    				<li>e-commerce</li> 
+		    				<li>Wordpress</li> 
+		    				<li>Awesome Graphic Design</li> 
+		    			</ul> 
+		    		</div> 
+	    		</div>
+    			<article class=bar-graph-info>
+	    				<div class=inner-border></div>
+	    				<div class=container>
+		    				<div class=content>
+		    					
+		    				</div>
+		    			</div>
+		    		<noscript><strong>Please enable Javascript to see info about my skillset, or</strong></noscript>
+	    				
+		    		<a class=resume-download href="#"><strong>Download Full Resume (PDF)</strong></a>
+	    		</article>
+    		</section>
+    		
+    		<section id=portfolio>
+    			<div id=portfolio-timer class=semi-light></div>
+				<div id=portfolio-info  class="semi-light-text dark-semi-transparent"></div>
+				<nav id=portfolio-navigation></nav>
+    			<div id=grid-overlay></div>
+    			
+    			<ul id=portfolio-images class=semi-light-text>
+    				<li class=first>
+    					<img src="./img/image1_full_chairlift.jpg" alt=""/>
+    					<aside class=dark-semi-transparent>
+    						<h1>Recent Work</h1>
+    						<p>
+    							Welcome to my portfolio slide&shy;show!
+    						</p>
+    							Chairlift Development is a project I created in August 2011 to help me deliver robust front-end solutions. 
+    						</p>
+    						<a href="#" class=portfolio-link>Visit Site</a>
+    					</aside>
+    				</li>
+    				<li>
+    					<img src="./img/image2_letterpress_ChairliftStudio.jpg" alt=""/>
+    					<aside class=dark-semi-transparent>
+    						<h1>Chairlift Studio</h1>
+    						<p>
+    							Proudly presenting HTML5 and JavaScript!
+    						</p>
+    						<p>
+    							See the code rock some designer threads. jQuery makes me feel like Adobe Flash and Actionscript 3 is obsolete.
+    						</p>
+    						<a href="#" class=portfolio-link>Visit Site</a>
+    					</aside>
+    				</li>
+    				<li><img src="./img/image1.jpg" alt=""/></li>
+    				<li>
+    					<img src="./img/image1_full_chairlift.jpg" alt=""/>
+    					<aside class=dark-semi-transparent>
+    						<h1>AirFromThere</h1>
+    						<p>
+    							Info about AirFromThere needed.
+    						</p>
+    					</aside>
+    				</li>
+    				<li>
+    					<img src="./img/image1_full_chairlift.jpg" alt=""/>
+    					<aside class=dark-semi-transparent>
+    						<h1>Next Project</h1>
+    						<p>
+    							Have you tried clicking on the image?
+    						</p>
+    						<p>
+    							Notice how the image fades out starting
+    							from where you clicked.
+    						</p>
+    						<p>
+    							You can also use the navigation buttons
+    							at the bottom left to jump to any image
+    							in the slideshow.
+    						</p>
+    						<a href="#" class=portfolio-link>Visit Site</a>
+    					</aside>
+    				</li>	 
+    				<li>
+    					<img src="./img/image1_full_chairlift.jpg" alt=""/>
+    					<aside class=dark-semi-transparent>
+    						<h1>Next Project</h1>
+    						<p>
+    							About to fill this box out with some dope info about my work!
+    						</p>
+    						<p>
+    							Word up.
+	    					</p>
+	    					<p>
+	    						Blah blah blah
+    						</p>
+    						<a href="#" class=portfolio-link>Visit Site</a>
+    					</aside>
+    				</li>
+    			</ul>
+    			<noscript>
+    				<div class="dark-semi-transparent semi-light-text">Please enable Javascript to see more of my projects.</div>
+    			</noscript>
+    		</section>
+    		
+    		<section id=contact>
+				<h1 class=dark-text>Contact Me</h1>
+				<form action="sendform.php" method=post id=contact-form>
+					<div class=inner-border></div>
+					<input type=text name=name id=contact-name placeholder=Name size=30 />
+					<span class=error id=name-error>Invalid name</span> <br/>
+					<input type=text name=email id=contact-email placeholder=Email size=30 />
+					<span class=error id=email-error>Invalid email</span>
+					<textarea name=message id=contact-message cols=65 rows=10 placeholder="Message..."></textarea>
+					<input type=submit name=submit value=Submit id=contact-submit class="semi-dark semi-light-text"/>
+					<span class=error id=message-error>Type in a message first</span>
+					<span class=success id=message-success>I'll get back to you soon!</span>
+				</form>
+				
+    			<div id=contact-info>
+    				<div class=inner-border></div>
+    				<ul class=semi-dark-text>
+    					
+    					<li><img src="./img/icons/UI/map_pin_stroke.png" alt=Location />Upper East Side <br/> <span class=second-row>New York, NY</span></li>
+    					<li><img src="./img/icons/UI/iphone.png" alt=Phone />(646) 543-6717</li>
+    					<li><a href="mailto:bcardan@gmail.com"><img src="./img/icons/UI/mail.png" alt=Email />bcardan@gamil.com</a></li>
+    					<li><a href="http://chairliftdevelopment.com/"><img src="./img/icons/UI/home.png" alt=Homepage />chairliftdevelopment.com</a></li>
+    					<li><a href="#"><img src="./img/icons/UI/user_alt.png" alt=vCard /><strong>Download My vCard</strong></a></li>
+    				</ul>
+    			</div>
+    		</section>
+	    </div>
+    </div>
+    <footer>
+		&copy; Copyright 2011. <em><a href="http://chairliftdevelopment.com/">Chairlift Development</a></em> by <a href="http://bent.chairliftdevelopment.com">Bent Cardan</a>
+    </footer>
+  </div> 
+
+
+  
+
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  <script>window.jQuery||document.write('<script src="./js/libs/jquery-1.6.2.min.js"><\/script>');</script>
+
+<script>
+  $(document).ready(function() {
+    $("#settings").remove();
+  });
+</script>
+
+
+  <script defer src='./js/1147a0aafa482b16c7e786125464caf90ba7c02c.js'></script>
+
+	
+  
+  <script>window._gaq=[["_setAccount","UA-25753152-1"],["_trackPageview"],["_trackPageLoadTime"]];Modernizr.load({load:("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js"});</script>
+
+
+  
+  <!--[if lt IE 7 ]><script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})});</script><![endif]-->
+  
+</body>
+</html>
